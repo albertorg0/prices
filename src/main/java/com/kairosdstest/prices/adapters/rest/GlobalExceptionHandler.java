1301a79abed554192491d7c5b79f5a9cd4490198
Alberto Rojas <a.rojas0001@gmail.com>
2024-03-10 12:54:36 +0100

    feat(handler): introduce custom error response object

--- src/main/java/com/kairosdstest/prices/adapters/rest/GlobalExceptionHandler.java
+++ src/main/java/com/kairosdstest/prices/adapters/rest/GlobalExceptionHandler.java
@@ -2,32 +2,35 @@ package com.kairosdstest.prices.adapters.rest;
 
 import com.kairosdstest.prices.core.NoResultsException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 /**
  * Global exception handler for the REST layer.
  */
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<String> handleMissingServletRequestParameter(MissingServletRequestParameterException ex) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingServletRequestParameter(MissingServletRequestParameterException ex) {
         String errorMessage = "Missing parameter: " + ex.getParameterName();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+        return ErrorResponse.builder().code(HttpStatus.BAD_REQUEST.value()).message(errorMessage).build();
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<String> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
         String errorMessage = "Invalid parameter type: " + ex.getParameter().getParameterName();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+        return ErrorResponse.builder().code(HttpStatus.BAD_REQUEST.value()).message(errorMessage).build();
     }
 
     @ExceptionHandler(NoResultsException.class)
-    public ResponseEntity<String> handleNoResults(NoResultsException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNoResults(NoResultsException ex) {
+        return ErrorResponse.builder().code(HttpStatus.NOT_FOUND.value()).message(ex.getMessage()).build();
     }
 }
